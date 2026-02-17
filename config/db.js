0cc77faf0a05be0802cc6548c0f4d8a8a80b0a62
Nkemaze <nkemazebless58@gmail.com>
2026-02-17 19:57:32 +0100

    removing consol.log for uri

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try{
         await mongoose.connect(process.env.MONGO_URI);
         console.log("MONGO_URI:", process.env.MONGO_URI);
-        console.log("MongoDB connected");
+        // console.log("MongoDB connected");
     } catch (error){
         console.error("Failed to connect ", error.message);
         process.exit(1);
